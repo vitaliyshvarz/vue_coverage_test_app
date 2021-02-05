df4abd4509e15dc0eb08609b4b138e72f8f63ec6
Vitaliy Shvarz <>
2021-02-05 15:27:18 +0100

    APP-123: fix tests

--- tests/unit/AddTodo.spec.js
+++ tests/unit/AddTodo.spec.js
@@ -1,10 +1,49 @@
-import { shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex';
+
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import AddTodo from '@/components/AddTodo.vue'
 
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+
 describe('AddTodo.vue', () => {
+  let actions;
+  let store;
+
+  beforeEach(() => {
+    actions = {
+      addTodo: jest.fn()
+    };
+
+    store = new Vuex.Store({
+      modules: {
+        todos: {
+          state: {},
+          actions: actions,
+          getters: {},
+          mutations: {}
+        }
+      }
+    });
+  })
+
   it('renders h3 when passed', () => {
     const h3 = 'Add Todo'
-    const wrapper = shallowMount(AddTodo)
+    const wrapper = shallowMount(AddTodo, { store, localVue })
     expect(wrapper.text()).toMatch(h3)
+  })
+
+  it('calls addTodo on onSubmit', () => {
+    const wrapper = shallowMount(AddTodo, { store, localVue })
+
+    const form = wrapper.find('form');
+
+    console.log(form);
+
+    form.trigger('submit');
+
+    expect(actions.addTodo).toHaveBeenCalled()
   })
 })
